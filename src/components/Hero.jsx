f6221f6965c07bb0e52d6d566d63dc0b90222531
egasa21 <esaputra168@.com>
2023-04-24 00:20:38 +0700

    update link url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                 <button
                     type='button'
                     onClick={() =>
-                        window.open("https://github.com/TidbitsJS/Summize", "_blank")
+                        window.open("https://github.com/egasa21/ai-articles-summarizer", "_blank")
                     }
                     className='black_btn'
                 >
